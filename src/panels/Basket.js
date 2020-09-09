5a171ae68ed9ddf9ea7f72b187ca953b305fcc45
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2020-09-09 22:23:10 +0300

    Initial commit

---
+++ src/panels/Basket.js
@@ -0,0 +1,178 @@
+import React, { useMemo, useState, useEffect } from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import accounting from 'accounting';
+
+import Checkbox from './Checkbox';
+
+import edit from '../img/edit.svg';
+import './place.css';
+
+
+const Basket = ({ match: { params: { areaId, itemId }}, foodAreas, order, details, setDetails }) => {
+
+  const [ faster, setFaster ] = useState(details.time == null || details.faster);
+  const [ time, setTime ] = useState(details.faster ? '' : details.time || '');
+  const [ selfService, setSelfService ] = useState(details.selfService || false);
+  const area = foodAreas.filter(area => area.id === areaId)[0];
+  const item = area.items.filter(item => item.id === itemId)[0];
+
+  useEffect(() => {
+    setDetails({faster, time, selfService})
+  }, [])
+
+  useEffect(() =>{
+    setDetails({faster, time, selfService})
+  }, [ faster, time, selfService])
+
+  const [ price, products ] = useMemo(() => {
+    const foodIds = new Set((item.foods || []).map(item => item.id));
+
+    const products = Object.values(order)
+      .filter((value) => {
+        const { item: { id }} = value;
+
+        return foodIds.has(id);
+      });
+
+    const result = products.reduce((result, value) => {
+        const { count, item } = value;
+
+        return result + parseInt(item.price) * parseInt(count);
+      }, 0);
+
+    return [ accounting.formatNumber(result, 0, ' '), products ];
+  }, [ order, item ]);
+
+  return (
+    <div className="Place">
+      <header className="Place__header">
+        <aside className="Place__trz">
+          <h1 className="Place__head">
+            <Link to="/" className="Place__logo">
+              {area.name}
+            </Link>
+          </h1>
+          <Link to="/edit" className="Place__change-tz">
+            <img
+              alt="change-profile"
+              src={edit}
+            />
+          </Link>
+        </aside>
+      </header>
+      <aside className="Place__restoraunt">
+        <img
+          className="Place__restoraunt-logo"
+          alt="Fastfood logo"
+          src={item.image}
+        />
+        <h2
+          className="Place__restoraunt-name"
+        >
+          {item.name}
+        </h2>
+        <p className="Place__restoraunt-type">
+          {item.description}
+        </p>
+      </aside>
+      <div className="Place__products-wrapper">
+        <ul className="Place__products">
+          {products.map(({ item, count }) => (
+            <li
+              className="Place__product"
+              key={item.id}
+            >
+              <img
+                className="Place__product-logo"
+                alt="Ordered product logo"
+                src={item.image}
+              />
+              <h3
+                className="Place__product-name"
+              >
+                {item.name}
+              </h3>
+              <p
+                className="Place__product-price"
+              >
+                Цена: {item.price}
+              </p>
+              <p
+                className="Place__product-count"
+              >
+                x{count}
+              </p>
+            </li>
+          ))}
+        </ul>
+        <Link
+          className="Place__change-product"
+          to={`/place/${areaId}/${itemId}`}
+        >
+          Изменить
+        </Link>
+      </div>
+      <div className="Place__choice">
+        <h3>Время:</h3>
+        <div className="Place__choice-item">
+          <span>Как можно быстрее</span>
+          <Checkbox 
+            checked={faster} 
+            onToggle={() => {
+              if (faster) {
+                setFaster(false);
+              } else {
+                setTime('');
+                setFaster(true);
+              }
+            }}
+          />
+        </div>
+        <div className="Place__choice-item">
+          <span>Назначить</span>
+          <input
+            value={time}
+            onFocus={() => {
+              setFaster(false);
+            }}
+            onChange={event => {
+              setFaster(false);
+              const { target: { value } } = event
+              if (!/^\d{2}:$/.test(time) 
+              && (/^[0-1]\d$/.test(value) || /^2[0-3]$/.test(value))) {
+                setTime(value + ":");
+              } else if (/^[0-2]?$/.test(value) 
+                || /^[0-1]\d$/.test(value) 
+                || /^2[0-3]$/.test(value)
+                || /^\d{2}:$/.test(value)
+                || /^\d{2}:[0-5]$/.test(value)
+                || /^\d{2}:[0-5]\d$/.test(value)) {
+                setTime(value);
+              }
+            }}
+            onBlur={() => {
+              if (time) {
+                setFaster(false);
+              }
+            }}
+          />
+        </div>
+        <div className="Place__choice-item">
+          <h3>С собой</h3>
+          <Checkbox checked={selfService} onToggle={() => setSelfService(!selfService)} />
+        </div>
+        <div className="Place__choice-item">
+          <h3>На месте</h3>
+          <Checkbox checked={!selfService} onToggle={() => setSelfService(!setSelfService)} />
+        </div>
+      </div>
+      <footer className="Place__footer">
+        <Link to={`/order/${area.id}/${item.id}`} className={price > 0 && (faster || /^\d{2}:\d{2}$/.test(time)) ? "Place__order" : "Place__order Place__order-disabled"}>
+          Оплатить {price}
+        </Link>
+      </footer>
+    </div>
+  );
+};
+
+export default withRouter(Basket);
